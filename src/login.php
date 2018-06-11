447234252fb25157f8200c9f39c9cf961fabdd05
Kilian Chollet <chollet.kilian@gmail.com>
2018-06-11 16:19:20 +0200

    Login view

--- src/login.php
+++ src/login.php
@@ -1,5 +1,20 @@
 <?php
-require_once("./_lib/vendor/autoload.php");
-$loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
+/**
+ * Created by PhpStorm.
+ * User: kilianchollet
+ * Date: 11/06/2018
+ * Time: 10:00
+ */
+
+require_once("../_lib/vendor/autoload.php");
+
+$loader = new \Twig_Loader_Filesystem(__DIR__ . "/..");
 $twig = new \Twig_Environment($loader);
-echo $twig->render('login.twig', ['name' => "hello world"]);
+
+// Set a title
+$message = "It works !";
+
+echo $twig->render('twig/pages/login.html.twig', [
+    'title' => 'Authentification',
+    'connected' => false
+]);
--- src/page_with_nav_example.php
+++ src/page_with_nav_example.php
@@ -6,9 +6,9 @@
  * Time: 10:00
  */
 
-require_once("./_lib/vendor/autoload.php");
+require_once("../_lib/vendor/autoload.php");
 
-$loader = new \Twig_Loader_Filesystem(__DIR__);
+$loader = new \Twig_Loader_Filesystem(__DIR__ . "/..");
 $twig = new \Twig_Environment($loader);
 
 // Set a title
@@ -16,5 +16,5 @@ $message = "It works !";
 
 echo $twig->render('twig/pages/nav-example.html.twig', [
     'title' => 'Authentification',
-    'connected' => false
+    'connected' => true
 ]);
