e3594cc57795db6ef991727d0c09dd9681a93391
Lucas Masson <lucmasson97@gmail.com>
2018-06-11 14:41:23 +0200

    Register/login

--- login.php
+++ login.php
@@ -2,4 +2,4 @@
 require_once("./_lib/vendor/autoload.php");
 $loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new \Twig_Environment($loader);
-echo $twig->render('index.twig', ['name' => "hello world"]);
+echo $twig->render('login.twig', ['name' => "hello world"]);
