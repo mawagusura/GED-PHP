49377163653b24867380b96369233c0e07570cd5
Lucas Masson <lucmasson97@gmail.com>
2018-06-11 14:17:20 +0200

    Vendor

---
+++ register.php
@@ -0,0 +1,5 @@
+<?php
+require_once("./_lib/vendor/autoload.php");
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
+$twig = new \Twig_Environment($loader);
+echo $twig->render('register.twig');
