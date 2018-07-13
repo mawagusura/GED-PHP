d200293ea6e39a01e7624f53d3194649783d361a
clement <clement.jaworski@hotmail.com>
2018-07-13 22:56:47 +0200

    start implementing research
    able to find a file only with his name

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,8 +19,21 @@ class SearchController extends AbstractController
             'fichiers' => 'aucune recherche'
             ]);
     }
-    public function displayResearch(): Response
+    public function displayResearch(Request $req): Response
     {
+
+        
+        $filtre=$req->request->get('filtre');
+        print_r($filtre);
+
+
+        $sql="select * from doc where doc_name LIKE :doc_name;";
+        $params['doc_name'] ='%'.$filtre.'%';
+        $stmt = $this->getDoctrine()->getManager();
+        $stmt=$stmt->getConnection()->prepare($sql);
+        $stmt->execute($params);
+        $result=$stmt->fetchAll();
+        print_r($result);
         return $this->render('pages/search.html.twig',[
             'title' => "Recherche d'un fichier",
             'connected' => true,
